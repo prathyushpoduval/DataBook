9e3e8f93f801827353a76bfdbb314b3037c0f41a
Prathyush Poduval <prathyushppoduval@gmail.com>
2023-12-11 15:28:43 -0800

    basic communication architecture established

--- main.py
+++ main.py
@@ -1,18 +1,47 @@
 from multiprocessing import Process
 import node
+import socket
+import time
 
-def run_node(node_id, port, other_nodes):
-    node.start_node(node_id, port, other_nodes)
+def run_node(node_id, port):
+    node.start_node(node_id, port)
+
+def send_message(port, message):
+    try:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect(('localhost', port))
+            s.sendall(message.encode('utf-8'))
+            response = s.recv(1024)
+            print(f"Response from node: {response.decode('utf-8')}")
+    except ConnectionRefusedError:
+        print(f"Failed to connect to node on port {port}. Make sure the node server is running.")
 
 if __name__ == "__main__":
-    nodes = [5000, 5001, 5002]  # Port numbers for each node
+    nodes = [2000, 2001, 2002]
     processes = []
 
     for i, port in enumerate(nodes):
-        other_nodes = nodes[:i] + nodes[i+1:]
-        p = Process(target=run_node, args=(i, port, other_nodes))
+        p = Process(target=run_node, args=(i, port))
         p.start()
         processes.append(p)
 
+    # Print PIDs of all started processes
+    for i, process in enumerate(processes):
+        print(f"Node {i} started with PID: {process.pid}")
+
+    # Increase this time if the nodes are not ready
+    time.sleep(2)
+
+    try:
+        while True:
+            node_number = int(input("Enter node number (0, 1, 2): "))
+            message = input("Enter message: ")
+            if node_number in range(len(nodes)):
+                send_message(nodes[node_number], message)
+            else:
+                print("Invalid node number.")
+    except KeyboardInterrupt:
+        print("\nExiting...")
+
     for process in processes:
-        process.join()
+        process.terminate()
--- node.py
+++ node.py
@@ -1,26 +1,20 @@
 import socket
-import json
+import threading
 
-def start_node(node_id, port, other_nodes):
-    # Start the server to listen to other nodes
+def start_node(node_id, port):
+
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind(('localhost', port))
     server.listen()
 
-    # Database interaction code goes here
+    print(f"Node {node_id} started on port {port}")
 
-    # Example client to send a message to another node
-    def send_message(target_port, message):
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.connect(('localhost', target_port))
-            s.sendall(json.dumps(message).encode('utf-8'))
-
-    # Main loop for the server
     while True:
         conn, addr = server.accept()
+
         with conn:
             data = conn.recv(1024)
-            if not data:
-                break
-            # Handle received data
-            print(f"Node {node_id} received: {data.decode('utf-8')}")
+            if data:
+                message = data.decode('utf-8')
+                print(f"Node {node_id} received: {message}")
+                conn.sendall(f"Node {node_id} got your message: '{message}'".encode('utf-8'))
